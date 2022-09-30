c3fd33444e500ed872b0bd64bdfdd37202f69345
xiecl001 <xiecl001@cmft.com>
2022-09-30 11:34:27 +0800

    P17_LetterCombinationsOfAPhoneNumber

--- src/com/aqiu/P17_LetterCombinationsOfAPhoneNumber.java
+++ src/com/aqiu/P17_LetterCombinationsOfAPhoneNumber.java
@@ -6,15 +6,33 @@ import java.util.List;
 public class P17_LetterCombinationsOfAPhoneNumber {
     public static void main(String[] args) {
         Solution solution = new P17_LetterCombinationsOfAPhoneNumber().new Solution();
-
+        solution.letterCombinations("23");
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public List<String> letterCombinations(String digits) {
+        String[] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> list = new ArrayList<>();
 
+        public List<String> letterCombinations(String digits) {
+            if (digits.length() != 0)
+                func(new StringBuilder(), digits, 0);
+            return list;
+        }
+
+        public void func(StringBuilder sb, String digits, int index) {
+            if (sb.length() == digits.length()) {
+                list.add(sb.toString());
+                return;
+            }
+            int no = Integer.parseInt(String.valueOf(digits.charAt(index)));
+            for (int i = 0; i < mapping[no].length(); i++) {
+                sb.append(mapping[no].charAt(i));
+                func(sb, digits, index + 1);
+                sb.deleteCharAt(index);
+            }
+            return;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
-
 }
