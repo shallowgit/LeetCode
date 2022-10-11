62b3fc68550e29e64c6daa44f826877041b788ca
xiecl001 <xiecl001@cmft.com>
2022-10-11 12:19:26 +0800

    P49_GroupAnagrams

--- src/com/aqiu/P49_GroupAnagrams.java
+++ src/com/aqiu/P49_GroupAnagrams.java
@@ -2,6 +2,7 @@ package com.aqiu;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class P49_GroupAnagrams {
@@ -13,46 +14,16 @@ public class P49_GroupAnagrams {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<List<String>> groupAnagrams(String[] strs) {
-            Arrays.sort(strs);
-            List<String> strsList = new ArrayList<>();
+            HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
             for (String str : strs) {
-                strsList.add(str);
+                char[] ch = str.toCharArray();
+                Arrays.sort(ch);
+                String key = String.valueOf(ch);
+                if(!hashMap.containsKey(key))
+                    hashMap.put(key,new ArrayList<>());
+                hashMap.get(key).add(str);
             }
-            List<List<String>> list = new ArrayList<>();
-            while (!strsList.isEmpty()) {
-                List<String> nextList = new ArrayList<>();
-                for (String s : strsList) {
-                    nextList.add(s);
-                }
-                String temp = strsList.get(0);
-                ArrayList<String> arrayList = new ArrayList<>();
-                arrayList.add(temp);
-                int index = 0;
-                for (int j = 1; j < strsList.size() && strsList.get(j).length() == temp.length(); j++) {
-                    if (!strsList.get(0) .equals("")) {
-                        for (int k = 0; k < temp.length(); k++) {
-                            if (!strsList.get(j).contains(temp.charAt(k) + "")) {
-                                break;
-                            }
-                            if (k == temp.length() - 1) {
-                                arrayList.add(strsList.get(j));
-                                nextList.remove(j - index);
-                                index++;
-                            }
-                        }
-                    } else {
-                        if (strsList.get(j).equals("")) {
-                            arrayList.add(strsList.get(j));
-                            nextList.remove(j - index);
-                            index++;
-                        }
-                    }
-                }
-                nextList.remove(0);
-                list.add(arrayList);
-                strsList = nextList;
-            }
-            return list;
+            return new ArrayList<>(hashMap.values());
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ src/com/aqiu/P73_SetMatrixZeroes.java
@@ -0,0 +1,16 @@
+package com.aqiu;
+
+public class P73_SetMatrixZeroes {
+    public static void main(String[] args) {
+        Solution solution = new P73_SetMatrixZeroes().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void setZeroes(int[][] matrix) {
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
