9949c4ca753efaec9b00b80449f1cd6b1ee7ea54
xiecl001 <xiecl001@cmft.com>
2024-04-03 17:25:30 +0800

    2024/4/2

---
+++ src/com/aqiu/sql/_22_P596_ClassesMoreThan5Students.java
@@ -0,0 +1,12 @@
+package com.aqiu.sql;
+
+public class _22_P596_ClassesMoreThan5Students {
+    public static void main(String[] args) {
+        /*
+            select class
+            from Courses
+            group by class
+            having count(*) >4
+        */
+    }
+}
