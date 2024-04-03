6ddafd510687de1c964af5329300da5f2d87348a
xiecl001 <xiecl001@cmft.com>
2024-04-03 17:35:11 +0800

    2024/4/3

---
+++ src/com/aqiu/sql/_23_P1729_FindFollowersCount.java
@@ -0,0 +1,12 @@
+package com.aqiu.sql;
+
+public class _23_P1729_FindFollowersCount {
+    public static void main(String[] args) {
+        /*
+            select user_id, count(*) as followers_count
+            from Followers
+            group by user_id
+            order by user_id
+        */
+    }
+}
