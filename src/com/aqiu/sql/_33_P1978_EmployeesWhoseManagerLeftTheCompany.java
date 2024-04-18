f1e40305c883e6c93f9817fe864c28b54d2c27b6
xiecl001 <xiecl001@cmft.com>
2024-04-18 09:24:39 +0800

    2024/4/18

---
+++ src/com/aqiu/sql/_33_P1978_EmployeesWhoseManagerLeftTheCompany.java
@@ -0,0 +1,23 @@
+package com.aqiu.sql;
+
+public class _33_P1978_EmployeesWhoseManagerLeftTheCompany {
+    public static void main(String[] args) {
+        /*
+            select employee_id
+            from Employees
+            where salary < 30000
+            and manager_id is not null
+            and manager_id not in (select employee_id from Employees)
+            order by employee_id
+        */
+        /*
+            select e1.employee_id
+            from Employees e1
+            left join Employees e2 on e1.manager_id = e2.employee_id
+            where e1.salary < 30000
+            and e1.manager_id is not null
+            and e2.employee_id is null
+            order by e1.employee_id
+        */
+    }
+}
