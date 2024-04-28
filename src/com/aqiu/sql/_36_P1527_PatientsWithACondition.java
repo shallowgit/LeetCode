05c1696a77672aafa1c7ea8a39b83b95dc54be8c
xiecl001 <xiecl001@cmft.com>
2024-04-28 09:52:26 +0800

    2024/4/28

---
+++ src/com/aqiu/sql/_36_P1527_PatientsWithACondition.java
@@ -0,0 +1,21 @@
+package com.aqiu.sql;
+
+public class _36_P1527_PatientsWithACondition {
+    public static void main(String[] args) {
+        /*
+            分两种情况：I型糖尿病写在最前面[以DIAB1开头]和写在中间（前面会有一个空格）[以空格加DIAB1开头]
+
+        */
+        /*
+            select *
+            from Patients
+            where instr(conditions, ' DIAB1') > 0
+            or instr(conditions, 'DIAB1') = 1
+
+            select *
+            from Patients
+            where conditions like 'DIAB1%'
+               or conditions like '% DIAB1%'
+        */
+    }
+}
